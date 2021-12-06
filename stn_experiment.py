1bf58f35c534849b22d4c358a75b29459cf9a798
Gerard Riera Solà <griera95@gmail.com>
2021-12-06 21:48:15 +0100

    Made script for first part of experiment

---
+++ stn_experiment.py
@@ -0,0 +1,44 @@
+from omegaconf import DictConfig, OmegaConf
+import hydra
+
+from src.data.make_dataset import get_dataloader
+from src.models.make_model import get_model
+from src.models.train_model import train
+from src.models.test_model import test
+from src.visualization.visualize import print_results, print_training_evolution
+
+@hydra.main(config_path='./config', config_name='config')
+def main(cfg: DictConfig) -> None:
+    print(OmegaConf.to_yaml(cfg))
+
+    print('\n------CREATING TRAINING AND TEST DATASET------\n')
+
+    # get loaders for training and test datasets
+    train_loader = get_dataloader(cfg, 'train')
+    test_loader = get_dataloader(cfg, 'test')
+
+    print('\n------GENERATING MODEL------\n')
+
+    # generate untrained model
+    model = get_model(cfg)
+
+    print('\n------STARTING TRAINING------\n')
+
+    # train the model and get loss history
+    losses = train(cfg, model, train_loader)
+
+
+    # print training loss evolution
+    print_training_evolution(losses)
+
+    # evaluate model on test dataset and get metrics
+    cm, accuracy, test_loss = test(model, test_loader)
+
+
+    # print metrics (confusion matrix, accuracy and loss)
+    print_results(cm, accuracy, test_loss)
+
+
+
+if __name__ == "__main__":
+    main()
