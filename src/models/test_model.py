4d09aee3cde958666c10ad0520f21259d814e52e
Gerard Riera Solà <griera95@gmail.com>
2021-12-06 10:56:22 +0100

    Added testing functionalities

---
+++ src/models/test_model.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn.functional as F
+from sklearn.metrics import confusion_matrix
+
+def test(model, test_loader):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    with torch.no_grad():
+        model.eval()
+        test_loss = 0
+        correct = 0
+        cms = []
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+
+            # sum up batch loss
+            test_loss += F.nll_loss(output, target, size_average=False).item()
+            # get the index of the max log-probability
+            pred = output.max(1, keepdim=True)[1]
+            cms.append(confusion_matrix(torch.flatten(target), torch.flatten(pred), labels=list(range(0, 10))))
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+        test_loss /= len(test_loader.dataset)
+        accuracy = correct / len(test_loader.dataset)
+        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'
+              .format(test_loss, correct, len(test_loader.dataset), 100. * accuracy))
+
+    return cms, accuracy
