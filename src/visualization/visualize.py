a104fb81682a0ae02fade7e1896ef25f9a9f08d7
Gerard Riera Solà <griera95@gmail.com>
2021-12-06 18:40:17 +0100

    Also show loss when printing results

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,11 +1,11 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def print_results(cm, accuracy):
+def print_results(cm, accuracy, test_loss):
     fig = plt.figure(figsize=(8, 6))
     plt.clf()
     res = sns.heatmap(cm, annot=True, fmt='d')
-    plt.title(f'Confusion Matrix. Accuracy: {100 * accuracy}%')
+    plt.title(f'Confusion Matrix. Accuracy: {100 * accuracy}%, test loss: {test_loss:.6f}')
     plt.xlabel('Predicted label')
     plt.ylabel('True label')
     plt.show()
